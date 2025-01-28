f097119b815b13762453ca11a5fcf035a78ff39b
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-28 09:44:39 +0700

    wrap partai in JPA entity

--- src/main/java/com/allobank/allobackendtest/model/Partai.java
+++ src/main/java/com/allobank/allobackendtest/model/Partai.java
@@ -1,12 +1,25 @@
 package com.allobank.allobackendtest.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Entity
+@Table(name = "partai")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Partai {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(name = "nama_partai")
     private String namaPartai;
+    @Column(name = "nomor_urut")
     private Integer nomorUrut;
 }
