e5fc1d3057f081f470ab360aa87b038de84a4532
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-28 10:06:14 +0700

    add caleg rest controller

---
+++ src/main/java/com/allobank/allobackendtest/controller/CalegController.java
@@ -0,0 +1,52 @@
+package com.allobank.allobackendtest.controller;
+
+import com.allobank.allobackendtest.model.dto.CalegDto;
+import com.allobank.allobackendtest.model.errors.GenericResponse;
+import com.allobank.allobackendtest.service.CalegService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("calegs")
+@RequiredArgsConstructor
+public class CalegController {
+
+    private final CalegService calegService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenericResponse<CalegDto>> getCalegById(@PathVariable UUID id) {
+        return ResponseEntity.ok(GenericResponse.success(calegService.findCalegById(id)));
+    }
+
+    @GetMapping("")
+    public ResponseEntity<GenericResponse<List<CalegDto>>> getAllCalegs(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(name="partai", required = false) UUID partaiId,
+            @RequestParam(name="dapil", required = false) UUID dapilId,
+            @RequestParam(defaultValue = "nomor_urut,asc") String sort
+    ) {
+        Sort sorting;
+        if(sort.contains(",")) {
+            String [] sortSplit = sort.split(",");
+            sorting = Sort.by(sortSplit[1].equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC, sortSplit[0]);
+        }else{
+            sorting = Sort.by(Sort.Direction.ASC, sort);
+        }
+
+        if (partaiId != null && dapilId != null) {
+            return ResponseEntity.ok(GenericResponse.success(calegService.findAllByPartaiIdAndDapilIdPageable(partaiId, dapilId, page, size, sorting)));
+        } else if(partaiId != null) {
+            return ResponseEntity.ok(GenericResponse.success(calegService.findAllByPartaiIdPageable(partaiId, page,size, sorting)));
+        }else if (dapilId != null) {
+            return ResponseEntity.ok(GenericResponse.success(calegService.findAllByDapilId(dapilId, page, size, sorting)));
+        } else {
+            return ResponseEntity.ok(GenericResponse.success(calegService.findAllCalegsPageable(page, size, sorting)));
+        }
+    }
+}
