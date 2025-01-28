fc4023c7a07ff1fbf7faa4fcd2516719edce9d3d
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2025-01-28 09:49:29 +0700

    adding schema initialization for test db

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,42 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS dapil
+(
+    id           UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    nama_dapil   VARCHAR(255),
+    provinsi     VARCHAR(255),
+    jumlah_kursi INT
+);
+
+CREATE TABLE IF NOT EXISTS partai
+(
+    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    nama_partai VARCHAR(255),
+    nomor_urut  INT
+);
+
+CREATE TABLE IF NOT EXISTS caleg
+(
+    id            UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    dapil_id      UUID NOT NULL,
+    partai_id     UUID NOT NULL,
+    nomor_urut    INT,
+    nama          VARCHAR(255),
+    jenis_kelamin VARCHAR(50),
+    FOREIGN KEY (dapil_id) REFERENCES dapil (id),
+    FOREIGN KEY (partai_id) REFERENCES partai (id)
+);
+
+CREATE TABLE IF NOT EXISTS wilayah_dapil
+(
+    dapil_id     UUID NOT NULL DEFAULT uuid_generate_v4(),
+    nama_wilayah VARCHAR(255),
+    PRIMARY KEY (dapil_id, nama_wilayah),
+    FOREIGN KEY (dapil_id) REFERENCES dapil (id)
+);
+
+CREATE INDEX idx_caleg_dapil_id ON caleg (dapil_id);
+CREATE INDEX idx_caleg_partai_id ON caleg (partai_id);
+CREATE INDEX idx_wilayah_dapil_id ON wilayah_dapil (dapil_id);
+
+ALTER TABLE caleg
+    ADD CONSTRAINT chk_jenis_kelamin CHECK (jenis_kelamin IN ('LAKILAKI', 'PEREMPUAN'));
